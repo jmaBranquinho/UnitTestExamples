ba8aa6b3e05793b98faee106bafa39936fd5b8af
Jorge Branquinho <31412379+jmaBranquinho@users.noreply.github.com>
2022-05-07 16:18:32 +0100

    Added bogus and xml stuff

---
+++ DataCreationExamples/Bogus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Bogus;
+using Xunit;
+
+namespace DataCreationExamples;
+
+public class Bogus
+{
+    [Fact]
+    public void FakeAPerson()
+    {
+        var personGenerator = new Faker<FakePerson>()
+            .RuleFor(p => p.Name, f => f.Person.FullName)
+            .RuleFor(p => p.Id, Guid.NewGuid)
+            .RuleFor(p => p.Email, f => f.Person.Email)
+            .RuleFor(p => p.Credits, f => f.Finance.Amount());
+        
+        var person = personGenerator.Generate(1);
+        ; // place breakpoint and check the data. Names and emails should be consistent, as well as other data
+    }
+}
+
+public class FakePerson
+{
+    public string Name { get; set; }
+
+    public Guid Id { get; set; }
+
+    public string Email { get; set; }
+
+    public decimal Credits { get; set; }
+}
---
+++ DataCreationExamples/XmlExamples.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using Xunit;
+
+namespace DataCreationExamples;
+
+public class XmlExamples
+{
+    [Fact]
+    public void LoadXmlFromFile()
+    {
+        // you can edit the xml file in the editor, load it like this and apply it to the unit tests 
+        // make sure the file is being copied on build
+        var xml = XElement.Load(@"someXMl.xml");
+        ; // place a breakpoint and check the xml
+    }
+    
+    [Fact]
+    public void LoadXmlFromString()
+    {
+        // this xml could be in a string variable or loaded from file
+        var xmlAsString = File.ReadAllText(@"someXMl.xml");
+
+        var conversion = new XmlDocument();
+        conversion.LoadXml(xmlAsString);
+        var xml = conversion.InnerXml;
+        ; // place a breakpoint and check the xml
+    }
+}
